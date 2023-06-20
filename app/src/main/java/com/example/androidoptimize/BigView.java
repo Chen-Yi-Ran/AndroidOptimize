6e0d546156b958fb5dbcbabb6df3337b927a6c9a
ChenYiRan1212 <1904201399@qq.com>
2023-06-20 20:22:12 +0800

    解决了惯性滑动时还没滑完图片就自动到达最末尾的位置的bug

--- app/src/main/java/com/example/androidoptimize/BigView.java
+++ app/src/main/java/com/example/androidoptimize/BigView.java
@@ -71,8 +71,8 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
 
         mImageWidth = mOptions.outWidth;
         mImageHeight = mOptions.outHeight;
-        Log.d(TAG, "mImageWidth: --->"+mImageWidth);
-        Log.d(TAG, "mImageHeight: --->"+mImageHeight);
+      //  Log.d(TAG, "mImageWidth: --->"+mImageWidth);
+      //  Log.d(TAG, "mImageHeight: --->"+mImageHeight);
         //开启内存复用
         mOptions.inMutable=true;
 
@@ -95,6 +95,7 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        Log.d(TAG, "执行onMeasure: ");
         mViewWidth = getMeasuredWidth();
         mViewHeight = getMeasuredHeight();
         Log.d(TAG, "mViewWidth: --->"+mViewWidth);
@@ -114,8 +115,9 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Log.d(TAG, "执行onDraw: ");
         if(mDecoder==null){
-            Log.d(TAG, "null: ---->");
+         //   Log.d(TAG, "null: ---->");
             return;
         }
         //内存复用
@@ -126,9 +128,9 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
         //Log.d(TAG, "mBitmap--->"+mBitmap);
         //计算缩放因子
         mScaleX = mViewWidth / (float) mImageWidth;
-        Log.d(TAG, "mScaleX: "+mScaleX);
+      //  Log.d(TAG, "mScaleX: "+mScaleX);
         mScaleY = mViewHeight / (float) mImageHeight;
-        Log.d(TAG, "mScaleY: "+mScaleY);
+      //  Log.d(TAG, "mScaleY: "+mScaleY);
         //得到矩阵缩放
         Matrix matrix = new Matrix();
         matrix.setScale(mScaleX, mScaleX);//如果matrix.setScale(mScaleX, mScaleY)则图片会在充满在当前的view的x和y轴
@@ -138,21 +140,24 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
     //第五步 处理点击事件
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        Log.d(TAG, "onTouch: ");
+        Log.d(TAG, "执行onTouch: ");
         //将Touch事件传递给手势
         return mGestureDetector.onTouchEvent(event);
     }
 
     //第六步 处理手势按下事件
+
     @Override
     public boolean onDown(MotionEvent e) {
-        Log.d(TAG, "onDown: ");
+        Log.d(TAG, "执行onDown: ");
         //如果滑动没有停止就 强制停止
         if(!mScroller.isFinished()){
             mScroller.forceFinished(true);
         }
-
         //将事件进行传递，接收后续事件
+        //因为在GestureDetector中，onDown方法是用于监听手指按下事件的，如果不返回true消费该事件，
+        // GestureDetector就不会将后续的事件传递给其他的方法进行处理，
+        // 包括滑动事件。因此，如果要实现按下手指后进行滑动图片的效果，需要在onDown方法中返回true进行消费。
         return true;
     }
 
@@ -161,34 +166,37 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
     //e2 即时事件也就是滑动时
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        Log.d(TAG, "onScroll----->distanceY--->: "+distanceY);
-        Log.d(TAG, "onScroll----->mImageHeight--->: "+mImageHeight);
+        Log.d(TAG, "执行onScroll: ");
+
         //上下滑动时，直接改变Rect的显示区域
         mRect.offset(0,(int) distanceY);//上下滑动只需要改变Y轴
+        Log.d(TAG, "onScroll----->distanceY--->: "+distanceY);
+        Log.d(TAG, "onScroll----->mRect.bottom--->: "+mRect.bottom);
         //判断到顶和到底的情况
         if(mRect.bottom>mImageHeight){//滑到最底
+            Log.d(TAG, "onScroll: 经来了");
             mRect.bottom=mImageHeight;
             mRect.top=mImageHeight-mViewHeight;
         }
-        Log.d(TAG, "mRect.top: "+mRect.top);
+      //  Log.d(TAG, "mRect.top: "+mRect.top);
         if(mRect.top<0){//滑到最顶
             mRect.top=0;
             mRect.bottom=mViewHeight;
         }
         invalidate();
-        return false;
+        return true;
     }
 
 
     //第八步 处理惯性问题(手势)指手势的滑动
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        Log.d(TAG, "onFling: ");
+       Log.d(TAG, "执行onFling: ");
         //velocityY表示Y轴的惯性值，startX和startY为滑动的开始位置,minY和maxY为滑动距离的最小值和最大值
-        Log.d(TAG, "onFling: mRect.top -->"+mRect.top);
+      //  Log.d(TAG, "onFling: mRect.top -->"+mRect.top);
         mScroller.fling(0,mRect.top,0,(int) -velocityY,0,0,0,mImageHeight-mViewHeight);
-        Log.d(TAG, "velocityY: "+velocityY);
-        Log.d(TAG, "mRect.top");
+     //   Log.d(TAG, "velocityY: "+velocityY);
+     //   Log.d(TAG, "mRect.top");
         return false;
     }
 
@@ -196,7 +204,7 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
     @Override
     public void computeScroll() {
         super.computeScroll();
-        Log.d(TAG, "computeScroll: ");
+       Log.d(TAG, "执行computeScroll: ");
         //如果没有滚动，直接返回即可
         if(mScroller.isFinished()){
             return;
@@ -205,7 +213,7 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
         if(mScroller.computeScrollOffset()){
             mRect.top=mScroller.getCurrY();
             Log.d(TAG, "computeScroll:mRect.top "+mRect.top);
-            mRect.bottom=mImageHeight-mViewHeight+mRect.top;//底部边框等于更新的top位置加上
+            mRect.bottom=mRect.top+mViewHeight;//底部边框等于更新的top位置加上
             Log.d(TAG, "computeScroll:mRect.bottom "+mRect.bottom);
         }
         invalidate();
@@ -225,8 +233,5 @@ public class BigView extends View implements GestureDetector.OnGestureListener, 
     public void onLongPress(MotionEvent e) {
 
     }
-
-
-
 
 }
--- app/src/main/java/com/example/androidoptimize/MainActivity.java
+++ app/src/main/java/com/example/androidoptimize/MainActivity.java
@@ -1,28 +1,31 @@
 package com.example.androidoptimize;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BigView bigView= findViewById(R.id.bigView);
+//        bigView.setOnClickListener();
         InputStream is=null;
         try {
-            is= getResources().getAssets().open("bigpicture.jpg");
+            is= getResources().getAssets().open("test.jpg");
             bigView.setImage(is);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //TODO: 1.通过Url从服务器获取图片 2.可以将图片进行传入，直接将图片进行显示 2.加入图片缓冲功能
+
 
 
     }
